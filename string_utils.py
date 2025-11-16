75aa9e168ccc2c823ba52fc023821340419b9282
Waleedshini <walid.shini@live.biu.ac.il>
2025-11-16 16:06:18 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,20 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+        for i in range(1, len(formula)):
+        if formula[i].isdigit():
+            return formula[:i], int(formula[i:])
+    return formula, 1 
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+        result = []
+    current_part = ""
+    for char in formula:
+        if char.isupper():
+            if current_part:
+                result.append(current_part)
+            current_part = char
+        else:
+            current_part += char
+    if current_part:
+        result.append(current_part)
+    return result
